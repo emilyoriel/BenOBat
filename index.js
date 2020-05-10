1f401fec5e409e0b9e9caf82fc2c846aa42adf98
Emily Oriel <58639088+emilyoriel@users.noreply.github.com>
2020-05-10 14:54:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+alert("ברוך הבא לאתר המצחיק שלי");
+
+
+document.getElementById("N").addEventListener("click", function(){
+    var audio = new Audio('n.mp3');
+    audio.play();
+  });
+
+  document.getElementById("E").addEventListener("click", function(){
+    var audio = new Audio('E.wav');
+    audio.play();
+  });
+
+  document.getElementById("Y").addEventListener("click", function(){
+    var audio = new Audio('y.mp3');
+    audio.play();
+  });
+
+  document.getElementById("S").addEventListener("click", function(){
+    var audio = new Audio('s.mp3');
+    audio.play();
+  });
